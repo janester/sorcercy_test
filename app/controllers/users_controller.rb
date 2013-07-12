3a3f207ccc50096835ff0e7448ae460c7fdeaa46
Jane Sternbach <janesternbach@gmail.com>
2013-07-11 22:32:25 -0700

    completed sorcery example

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  before_filter :require_login, :only => :show
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_path, :notice => "signed up!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
